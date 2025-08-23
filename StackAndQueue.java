242333b04635a1e8187dc10e5f3fb400da8f412c
Mudassir Qureshi <mudassirmohammad7987@gmail.com>
2025-08-23 23:57:19 +0530

    Java Stack and Queue

--- StackAndQueue.java
+++ StackAndQueue.java
@@ -11,5 +11,23 @@ public class StackAndQueue {
         System.out.println("Stack: " + st);
 
         System.out.println("Top element: " + st.peek());
+        System.out.println("Popped element: " + st.pop()); // pop also returns the top element unlike in C++
+        System.out.println("Stack after pop: " + st);
+
+
+        ArrayDeque<Integer> que = new ArrayDeque<>();
+        que.addLast(10); // enqueue
+        que.addLast(20);
+        que.addLast(30);
+        System.out.println("Queue: " + que);
+
+        que.addLast(40);
+        System.out.println("Queue after adding 40: " + que);
+
+        System.out.println("Front element: " + que.getFirst());  // getFirst does not remove the element unlike in C++
+        System.out.println("Queue after getFirst: " + que);     
+        
+        System.out.println("Removed element: " + que.removeFirst()); // dequeue
+        System.out.println("Queue after removeFirst: " + que);
   }
 }
